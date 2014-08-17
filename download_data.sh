82f23c3f9b7e3800ca0944f4e0ba77e8cb36dc0c
Flavio Truzzi <flaviotruzzi@gmail.com>
2014-08-16 23:43:49 -0300

    clean after parsing

--- download_data.sh
+++ download_data.sh
@@ -1,9 +1,17 @@
+# Download data
 wget http://www.camara.gov.br/cotas/AnoAtual.zip
 wget http://www.camara.gov.br/cotas/AnoAnterior.zip
 
+# unzip
 unzip AnoAtual.zip 
 unzip AnoAnterior.zip
 
+# parse
 cat AnoAtual.xml | tidy -utf8 -xml -w 255 -i -c -q -asxml | python parse.py > ano_atual.json 
 cat AnoAnterior.xml | tidy -utf8 -xml -w 255 -i -c -q -asxml | python parse.py > ano_anterior.json
 
+# clean it
+rm AnoAtual.zip
+rm AnoAnterior.zip
+rm AnoAtual.xml
+rm AnoAnterior.xml
