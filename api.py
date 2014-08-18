ff08e39b6c914f24d3850932db440dfd74d6e389
Flavio Truzzi <flaviotruzzi@gmail.com>
2014-08-17 23:49:33 -0300

    Adding lru_cache and rank by congressman. It is not a rank in fact...

--- api.py
+++ api.py
@@ -2,7 +2,7 @@
 # vim:ts=4:sts=4:sw=4:et:wrap:ai:fileencoding=utf-8:
 
 from flask import Flask
-from tools import INDEX, TYPE
+from tools import INDEX, TYPE, lru_cache
 import elasticsearch
 import json
 
@@ -13,9 +13,9 @@ es = elasticsearch.Elasticsearch()
 
 
 @app.route("/parties", methods=["GET"])
+@lru_cache(maxsize=1)
 def get_parties(with_counts=None):
     query = {
-        "query": {"match_all": {}},
         "aggs": {"parties": {"terms": {"field": "sgPartido"}}}
     }
     result = es.search(index=INDEX, doc_type=TYPE, body=query)
@@ -27,14 +27,37 @@ def get_parties(with_counts=None):
         return json.dumps([item["key"] for item in result])
 
 
+@app.route("/deputados", methods=["GET"])
+@lru_cache(maxsize=1)
+def get_congressmans(with_counts=None):
+    query = {
+        "aggs": {
+            "parties": {
+                "terms": {
+                    "field": "txNomeParlamentar",
+                    "size": 0
+                }
+            }
+        }
+    }
+    result = es.search(index=INDEX, doc_type=TYPE, body=query)
+    result = result["aggregations"]["parties"]["buckets"]
+
+    if with_counts:
+        return json.dumps(result)
+    else:
+        return json.dumps([item["key"] for item in result])
+
+
 # FIXME: names must be FULL to avoid problems.
-@app.route("/gasto/name/<name>", methods=["GET"])
+@app.route("/gasto/deputados/<name>", methods=["GET"])
+@lru_cache(maxsize=200)
 def get_spending_by_name(name):
     query = {
         "query": {
             "match": {"txNomeParlamentar": name}
         },
-        "aggs": {
+        "aggregations": {
             "spending": {"sum": {"field": "vlrLiquido"}}
         }
     }
@@ -44,12 +67,13 @@ def get_spending_by_name(name):
 
 
 @app.route("/gasto/party/<party>", methods=["GET"])
+@lru_cache(maxsize=20)
 def get_spending_by_party(party):
     query = {
         "query": {
             "match": {"sgPartido": party}
         },
-        "aggs": {
+        "aggregations": {
             "spending": {"sum": {"field": "vlrLiquido"}}
         }
     }
@@ -59,11 +83,19 @@ def get_spending_by_party(party):
 
 
 @app.route("/gasto/party", methods=["GET"])
-def get_spending_rank():
+@lru_cache(maxsize=1)
+def get_spending_rank_by_party():
     parties = get_parties()
     return json.dumps({party: get_spending_by_party(party) for
                        party in json.loads(parties)})
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/gasto/deputados", methods=["GET"])
+@lru_cache(maxsize=1)
+def get_spending_rank_by_congressman():
+    congressmans = get_congressmans()
+    return json.dumps({congressman: get_spending_by_name(congressman) for
+                       congressman in json.loads(congressmans)})
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
