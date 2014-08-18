c3635cab7c0c09f872e7db2107a254c1c2191b65
Flavio Truzzi <flaviotruzzi@gmail.com>
2014-08-17 23:47:58 -0300

    adding lru_cache to import

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
 # vim:ts=4:sts=4:sw=4:et:wrap:ai:fileencoding=utf-8:
 
-from parse_and_import import INDEX, TYPE
+from tools.parse_and_import import INDEX, TYPE
+from tools.cache import lru_cache
